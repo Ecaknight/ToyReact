2b93f00654b3a9c5646f320ae52168ab2d50dd60
v_ljianlliu <imooc156>
2020-07-21 22:00:09 +0800

    添加更新

--- main.js
+++ main.js
@@ -1,22 +1,70 @@
 // 架子验证页面
 import { ToyReact, Component } from './ToyReact'
+// import './style.css'
+// class MyComponent extends Component {
+//   render () {
+//     return (
+//       <div>
+//         <div>aaa</div>
+//         {this.children}
+//       </div>
+//     )
+//   }
+// }
 
-class MyComponent extends Component {
-  render () {
+// let a = <MyComponent name="a"><span>xxxx</span></MyComponent>
+
+class Square extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+  }
+  render() {
     return (
-      <div>
-        <div>aaa</div>
-        {this.children}
-      </div>
-    )
+      <button className="square" onClick={() => this.setState({ value: 'x' })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
-let a = <MyComponent name="a"><span>xxxx</span></MyComponent>
 
+class Board extends Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={i}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
 // document.body.appendChild(a) 
 // render方法
 ToyReact.render(
-  a,
+  <Board />,
   document.body,
 )
---
+++ summary.md
@@ -0,0 +1,16 @@
+# 记录
+
+### 安装
+
+```js
+npm i @babel/core @babel/plugin-transform-react-jsx @babel/preset-env babel-loader webpack webpack-cli -D
+```
+
+成功后在`package.json`中配置命令, 保证局部安装执行命令
+
+```json
+...
+"build": "webpack"
+...
+```
+
